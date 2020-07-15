9b5878a9a8c50476b3a0185748084792e0225f3c
niteshn99 <niteshn99@gmail.com>
2020-07-15 23:30:21 +0200

    changed contact form

--- static/js/environment.js
+++ static/js/environment.js
@@ -1,5 +1,7 @@
 let API_KEY;
 let THYROCARE_ORDER_ROOT_URL;
+let CONTACT_FORM_URL = "https://script.google.com/macros/s/AKfycbyxcZ4dD0CRK0KctqgoPsIXhy8LnDgL3vX52UhlAuYc_n8d_KQ/exec";
+
 document.addEventListener("DOMContentLoaded", function () {
 	let origin   = window.location.origin;
 	if (origin.indexOf("localhost") >= 0 ) {
